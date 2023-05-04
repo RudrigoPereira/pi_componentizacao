24d288567b1eac5518e81c2ffd6817fa0ac99e4a
RudrigoPereira <rudrigopereira@gmail.com>
2023-05-04 11:22:52 -0300

    tela de login

---
+++ src/login.js
@@ -0,0 +1,24 @@
+import "./login.css"
+
+export default function Login() {
+    return (
+      <div className="Login">
+        <form>
+            <label for="login">Login:</label>
+            <input type="text" id="username" name="username" placeholder="E-mail" required/>
+            <input type="password" id="password" name="password" placeholder="Senha" required/>
+            <a href="#" class="esqueceu">Esqueceu sua senha?</a>
+            <p>
+                <input type="submit" value="Entrar"/>
+            </p>
+
+            <div class="links">
+                Ainda não tem conta?
+                <a href="#"> Cadastre-se</a>
+            </div>
+        </form>
+      </div>
+    );
+  }
+  
+  
