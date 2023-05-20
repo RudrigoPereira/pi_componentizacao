bf4309e4a27e3abdf94be0f0514d524966dc15b9
Rodrigo Ferreira Pereira <45149332+RudrigoPereira@users.noreply.github.com>
2023-05-20 16:25:48 -0300

    componente das cadeiras do cinema terminado

--- src/cadeirasCine.js
+++ src/cadeirasCine.js
@@ -1,44 +1,60 @@
 import "./cadeirasCine.css";
 
 export default function CadeirasCine() {
-    const seats = Array.from({ length: 350 }, (_, index) => index + 1);
+  const seats = Array.from({ length: 180 }, (_, index) => index + 1);
 
-    const handleSeatClick = (seatNumber) => {
-        const seat = document.getElementById(`seat-${seatNumber}`);
-        if (seat && !seat.classList.contains('occupied')) {
-            seat.classList.toggle('selected');
-        }
-    };
+  const handleSeatClick = (seatNumber) => {
+    const seat = document.getElementById(`seat-${seatNumber}`);
+    if (seat && !seat.classList.contains("occupied")) {
+      seat.classList.toggle("selected");
+    }
+  };
 
-    return (
-        <div className="container">
-            <div className="seats">
-                {seats.map((seatNumber) => (
-                    <div
-                        key={seatNumber}
-                        id={`seat-${seatNumber}`}
-                        className={`seat`}
-                        onClick={() => handleSeatClick(seatNumber)}
-                    >
-                        {seatNumber}
-                    </div>
-                ))}
+  const isSpaceBetween = (seatNumber) => {
+    const spaceSeats = [8, 24, 40, 56, 72, 88, 104, 120, 136, 152, 168, 178];
+    return spaceSeats.includes(seatNumber);
+  };
+
+  const hasBottomMargin = (seatNumber) => {
+    return seatNumber >= 33 && seatNumber <= 48
+        || seatNumber >= 113 && seatNumber <= 128;
+  };
+
+  return (
+    <div className="container">
+      <div className="seats">
+        {seats.map((seatNumber) => {
+          const seatClassName = isSpaceBetween(seatNumber) ? "seat space" : "seat";
+          const seatStyle = hasBottomMargin(seatNumber) ? { marginBottom: "15px" } : {};
+
+          return (
+            <div
+              key={seatNumber}
+              id={`seat-${seatNumber}`}
+              className={seatClassName}
+              style={seatStyle}
+              onClick={() => handleSeatClick(seatNumber)}
+            >
+              {seatNumber}
             </div>
-            <div className="screen">Tela</div>
-            <div className="legend">
-                <div className="legend-item">
-                    <div className="seat available"></div>
-                    <span>Disponível</span>
-                </div>
-                <div className="legend-item">
-                    <div className="seat selected"></div>
-                    <span>Selecionada</span>
-                </div>
-                <div className="legend-item">
-                    <div className="seat occupied"></div>
-                    <span>Ocupada</span>
-                </div>
-            </div>
+          );
+        })}
+      </div>
+      <div className="screen">Tela</div>
+      <div className="legend">
+        <div className="legend-item">
+          <div className="seat available"></div>
+          <span>Disponível</span>
         </div>
-    );
+        <div className="legend-item">
+          <div className="seat selected"></div>
+          <span>Selecionada</span>
+        </div>
+        <div className="legend-item">
+          <div className="seat occupied"></div>
+          <span>Ocupada</span>
+        </div>
+      </div>
+    </div>
+  );
 }
